41a921738f53f3f0260a548d5a86f0f932353a8b
Chris Troutner <chris.troutner@gmail.com>
2025-08-26 11:12:51 -0700

    fix(blockchain.js): Adding blockchain adapter library

---
+++ src/adapters/blockchain.js
@@ -0,0 +1,32 @@
+/*
+  This is the adapter for the blockchain.
+*/
+
+class BlockchainAdapter {
+    constructor() {
+      this.chain = []
+      this.newTransactions = []
+  
+      // Bind 'this' object to all methods
+      this.createNewBlock = this.createNewBlock.bind(this)
+    }
+  
+    createNewBlock(inObj = {}) {
+      const {nonce, previousBlockHash, hash} = inObj
+  
+      const newBlock = {
+          index: this.chain.length + 1,
+          timestamp: new Date().toISOString(),
+          transactions: this.newTransactions,
+          nonce,
+          hash, // Hash of the newTransactions array.
+          previousBlockHash
+      }
+  
+      this.newTransactions = []
+  
+      return newBlock
+    }
+  }
+  
+  export default BlockchainAdapter
